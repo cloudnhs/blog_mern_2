6502a450ce69bc375a549f2a1f16dc13297dcda0
william <cloudnhs@gmail.com>
2020-07-22 21:14:28 +0900

    add register profile

--- routers/profile.js
+++ routers/profile.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
+const profileModel = require('../models/profile');
 const checkAuth = passport.authenticate('jwt',{session:false});
 
 // @route POST profile/
@@ -8,8 +9,41 @@ const checkAuth = passport.authenticate('jwt',{session:false});
 // @access Private
 
 router.post('/', checkAuth ,(req,res) => {
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (req.body.introduce) profileFields.introduce = req.body.introduce;
+    if (req.body.company) profileFields.company = req.body.company;
+    if (req.body.website) profileFields.website = req.body.website;
+    if (req.body.location) profileFields.location = req.body.location;
+    if (req.body.status) profileFields.status = req.body.status;
+    if (req.body.gender) profileFields.gender = req.body.gender;
+    if (req.body.age) profileFields.age = req.body.age;
+
+    if(typeof req.body.skills !== 'undefined'){
+        profileFields.skills = req.body.skills.split(',');
+    }
+
+    profileModel
+        .findOne({user: req.user.id})
+        .then(profile => {
+            if(profile){
+                return res.json({
+                    message : '등록된 프로필이 있습니다.'
+                })
+            }
+            new profileModel(profileFields)
+                .save()
+                .then(profile => res.json(profile))
+                .catch(err => res.json(err));
+        })
+        .catch(err => {
+            res.json({
+                message : err.models
+            })
+        })
 
 });
+
 
 
 
