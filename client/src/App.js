70464b25ad4140685d609a21aa84322f62c5a1ec
william <cloudnhs@gmail.com>
2020-08-01 11:35:18 +0900

    add Landing Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
+import Landing from "./components/layout/Landing";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
         <Navbar/>
-        <h1>Hello world!</h1>
+        <Landing/>
         <Footer/>
     </div>
   );
