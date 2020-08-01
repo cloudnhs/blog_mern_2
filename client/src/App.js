87daf927fa3b27daef4cb7bcf18530a76589241d
william <cloudnhs@gmail.com>
2020-08-01 11:15:19 +0900

    add navbar and footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <h1>Hello world!</h1>
+        <Navbar/>
+        <h1>Hello world!</h1>
+        <Footer/>
     </div>
   );
 }
