7f23522fa6840c7c565d946d9035ade2c0450c3a
william <cloudnhs@gmail.com>
2020-08-29 12:42:53 +0900

    add delete post

--- routers/post.js
+++ routers/post.js
@@ -40,6 +40,7 @@ router.post('/', checkAuth,(req,res) => {
 router.get('/', (req,res)=>{
     postModel
         .find()
+        .sort({date:-1})
         .populate("user", ["email", "avatar"])
         .then(posts => {
             if(posts.length === 0){
@@ -55,6 +56,62 @@ router.get('/', (req,res)=>{
         .catch(err => res.status(400).json(err));
 })
 
+// @route Get /post/detail
+// @desc  get detail post
+// @access Public
+router.get('/:postid',(req,res) => {
+    postModel
+        .findById(req.params.postid)
+        .then(post => {
+            if(!post){
+                return res.status(200).json({
+                    message : 'there is no post yet'
+                })
+            }
+            res.status(200).json(post);
+        })
+        .catch(err => res.status(400).json(err));
+})
+
+
+// @route delete /post
+// @desc delete  post
+// @access Private
+
+router.delete('/:postid', checkAuth, (req,res) => {
+
+    postModel
+        .findById(req.params.postid)
+        .then(post => {
+            console.log("------------->", post)
+            console.log(req.user.id);
+            console.log(post.user)
+            if(post.user.toString() === req.user.id ) {
+                post
+                    .remove()
+                    .then(() => {
+                        return res.json({
+                            message: req.params.postid +'is deleted'
+                        })
+                    })
+                    .catch(err => res.status(400).json(err))
+                // postModel
+                //     .findByIdAndDelete(req.params.postid)
+                //     .then(() => {
+                //         return res.json({
+                //         message: req.params.postid +'is deleted'}
+                //         )
+                //     })
+                //     .catch(err => res.status(400).json(err))
+            }
+            else{
+                return res.json({
+                    message: 'not allowed'
+                })
+            }
+        })
+        .catch(err => res.status(404).json(err))
+})
 
 
 module.exports = router;
